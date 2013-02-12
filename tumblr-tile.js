4ab22d50ad001c0751861e4ab52fac19f8f0682b
Takuya ASADA <syuu@dokukino.com>
2013-02-12 11:20:19 +0900

    change uri

--- tumblr-tile.js
+++ tumblr-tile.js
@@ -20,7 +20,7 @@ tumblrTile || (function() {
         var config    = configStr ? JSON.parse(configStr) : {};
 
         var defaultConfig = {
-            hostname : "aoi-miyazaki.tumblr.com",
+            hostname : "glenda9.tumblr.com",
             baseWidth: 250,
             margin   : 10
         };
